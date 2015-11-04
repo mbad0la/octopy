2749bc115cc0b1d4d42f74889c7ac49048a9b213
Mayank Badola <badola21295@gmail.com>
2015-11-04 18:48:06 +0530

    Update app_driver.py

--- app_driver.py
+++ app_driver.py
@@ -1,11 +1,29 @@
 import requests,json
+import follow
 
 class octoAPy(object):
+    
     def __init__(self,token):
+        
         self.AccessToken = token
         headers={'Authorization':'token '+self.AccessToken,'Accept':'application/json','Content-Type':'application/json'}
         r = requests.get('https://api.github.com/user',headers=headers)
         self.TokenBearer = json.loads(r.text)
 
     def owner(self):
+        
         return json.dumps(self.TokenBearer)
+        
+    def follower_driver(self,username = None):
+        f = follow.followAPI(self.AccessToken)
+        if username is None:
+            return f.get_followers(self.TokenBearer['login'])
+        else:
+            return f.get_followers(username)
+
+    def following_driver(self,username = None):
+        f = follow.followAPI(self.AccessToken)
+        if username is None:
+            return f.get_following(self.TokenBearer['login'])
+        else:
+            return f.get_following(username)
