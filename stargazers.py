a7f5803a90b5836d5a6328251011ccaf4abfd361
Mayank Badola <badola21295@gmail.com>
2015-11-16 23:30:52 +0530

    Update stargazers.py

--- stargazers.py
+++ stargazers.py
@@ -9,3 +9,8 @@ class starAPI(object):
         headers={'Authorization':'token '+self.AccessToken,'Accept':'application/json','Content-Type':'application/json'}
         r = requests.get("https://api.github.com/repos/"+RepoOwner+"/"+RepoName+"/stargazers",headers=headers)
         return json.dumps(r.text)
+        
+    def star_it(self,RepoOwner,RepoName):
+        headers={'Authorization':'token '+self.AccessToken,'Content-length':0,'Accept':'application/json','Content-Type':'application/json'}
+        r = requests.put("https://api.github.com/user/starred/"+RepoOwner+"/"+RepoName,headers=headers)
+        return json.dumps(r.status_code)
